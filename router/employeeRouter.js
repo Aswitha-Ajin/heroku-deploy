3083c960b59aa2a7bf9bb6daf998c4077aa3a8c9
Aswitha <108168862+Aswitha-Ajin@users.noreply.github.com>
2022-11-04 18:29:47 +0530

    Update employeeRouter.js

--- router/employeeRouter.js
+++ router/employeeRouter.js
@@ -12,12 +12,3 @@ router.delete("/delete/:id", employeeModule.deleteEmployees);
 
 module.exports = router;
 
-// const express=require("express");
-// const router=express.Router();
-// const employeeModule=require("../modules/employeeModule")
-// router.get("/get",employeeModule.getEmployees );
-// router.put("/update",employeeModule.updateEmployees);
-// router.post("/create",employeeModule.createEmployees);
-// router.delete("/delete",employeeModule.deleteEmployees);
-
-// module.exports=router;
