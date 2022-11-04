277083bfef093cc6e6f3d6f80f14556befba7792
Aswitha <108168862+Aswitha-Ajin@users.noreply.github.com>
2022-11-04 18:30:16 +0530

    Update employeeModule.js

--- modules/employeeModule.js
+++ modules/employeeModule.js
@@ -56,44 +56,4 @@ module.exports.deleteEmployees = async (req, res) => {
   }
 };
 
-// const {selectedDb}=require("../connect");
-// module.exports.getEmployees=async(req,res)=>{
-//     try{
-//     const employeesData=await mongo.selectedDb.collection("employees").find().toArray();
-//     res.send(employeesData);
-//     }catch(err){
-//     console.log(err)
-//     res.status(500).send(err)}
-    
-//     };
-//     module.exports.updateEmployees = async (req, res) => {
-//         try {
-//           const id = req.params.ids;
-//           const updatedData = await mongo.selectedDb
-//             .collection("employees")
-//             .findOneAndUpdate(
-//               { _id: ObjectId(id) },
-//               { $set: { ...req.body } },
-//               { returnDocument: "after" }
-//             );
-//           res.send(updatedData);
-//         } catch (err) {
-//           console.error(err);
-//           res.status(500).send(err);
-//         }
-//       };
-// module.exports.createEmployees=async(req,res)=>{
-//     try{
-//         console.log(req.body);
-//        const insertedResponse=await selectedDb.collection("employees").insertOne(req.body);
-//     res.send(insertedResponse);
-//     }catch(err){
-//         console.log("err");
-//         res.status(500).send(err);
-//     }
-
-// };
-// module.exports.deleteEmployees=(req,res)=>{
-//     res.send({status:"updated de" });
-// };
 
